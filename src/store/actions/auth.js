027b717d9301d74e14c235f8c384d158c24ff8a6
saishashaank <sai.shashaank@philips.com>
2019-04-14 11:13:34 +0530

    Fixed a bug for orders tab not opening

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCtjjyDYhNcKsCgOsukQszDOI1LJd-q5TY';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCtjjyDYhNcKsCgOsukQszDOI1LJd-q5TY';
         }
         axios.post(url, authData)
             .then(response => {
